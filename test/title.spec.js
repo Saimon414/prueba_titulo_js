cd13c17a1ae695e197f7babf82c63a1b6cd46ea0
Simon Valdez <simovaldez414@gmail.com>
2025-09-05 23:02:45 -0400

    primeros test UX

---
+++ test/title.spec.js
@@ -0,0 +1,25 @@
+const {test, expect} = require ('@playwright/test');
+
+test('valida el titulo exacto', async ({ page }) => {
+    //Abre la pagina example.com
+    await page.goto('https://example.com');
+    await sleep (3000);
+
+    //Verificacion del titulo exacto
+    await expect(page).toHaveTitle('Example Domain');
+}
+);
+
+test('valida titulo con regex (contiene palabra)', async ({ page }) => {
+    //Abre la pagina example.com
+    await page.goto('https://example.com');
+    await sleep (3000);
+
+    //Verificacion palabra
+    await expect(page).toHaveTitle(/Example/);
+}
+);
+
+function sleep (time){
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
